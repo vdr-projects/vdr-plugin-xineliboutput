4c2bbffbc0860dae39cf90bc92737f55945e41ec
Petri Hintukainen <phintuka@users.sourceforge.net>
2007-01-03 06:12:40 +0000

    Main menu entry text simplified

--- xineliboutput.c
+++ xineliboutput.c
@@ -45,7 +45,7 @@
 
 static const char *VERSION        = "1.0.0pre7";
 static const char *DESCRIPTION    = "X11/xine-lib output plugin";
-static const char *MAINMENUENTRY  = "Media Player (xine-lib)";
+static const char *MAINMENUENTRY  = "Media Player";
 
 cOsdObject *g_PendingMenuAction = NULL;
 
