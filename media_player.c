e19db127b081769c24448d1580fe89ed3e7457f0
Petri Helin <phelin@users.sourceforge.net>
2007-09-17 18:12:28 +0000

    Change the playlist behaviour so that kLeft and kPrev restart the current entry if it has been playing more than three seconds. Otherwise a previous one is selected.

--- media_player.c
+++ media_player.c
@@ -735,10 +735,17 @@ eOSState cXinelibPlayerControl::ProcessKey(eKeys Key)
                   MsgReplaying(*m_Player->Playlist().Current()->Track, *m_Player->File());
                   break;
     case kPrev:
-    case kLeft:   m_Player->NextFile(-1);
-                  if(!m_DisplayReplay)
-                    m_AutoShowStart = time(NULL);
-                  MsgReplaying(*m_Player->Playlist().Current()->Track, *m_Player->File());
+    case kLeft:   if(cXinelibDevice::Instance().PlayFileCtrl("GETPOS") < 3000) {
+                    m_Player->NextFile(-1);
+                    if(!m_DisplayReplay)
+                      m_AutoShowStart = time(NULL);
+                    MsgReplaying(*m_Player->Playlist().Current()->Track, *m_Player->File());
+                  }
+                  else {
+                    m_Player->NextFile(0);
+                    if(!m_DisplayReplay)
+                      m_AutoShowStart = time(NULL);
+                  }
                   break;
     case kDown:
     case kPause:  if(m_Player->Speed()) {
