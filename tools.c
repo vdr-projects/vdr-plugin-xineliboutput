362b053e78f81c3f5cb10e78b0148af5dc8fa50a
Petri Hintukainen <phintuka@users.sourceforge.net>
2008-02-04 23:05:34 +0000

    Initial import

---
+++ tools.c
@@ -0,0 +1,23 @@
+/*
+ * tools.c: VDR/C++ wrapper for common tools
+ *
+ * See the main source file 'xineliboutput.c' for copyright information and
+ * how to reach the author.
+ *
+ * $Id$
+ *
+ */
+
+#define __STDC_CONSTANT_MACROS
+#include <stdint.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+
+#include <vdr/tools.h>
+#include "logdefs.h"
+
+
+#include "tools/pes.c"
+#include "tools/mpeg.c"
+#include "tools/h264.c"
+
