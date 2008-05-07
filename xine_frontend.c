c4ec1935469db71fa29ad905da73371ee10c419e
Petri Hintukainen <phintuka@users.sourceforge.net>
2008-05-07 14:43:43 +0000

    Fix parsing pointer value

--- xine_frontend.c
+++ xine_frontend.c
@@ -1300,7 +1300,7 @@ static void *fe_control(frontend_t *this_gen, const char *cmd)
 
   } else if(!strncmp(cmd, "SLAVE 0x", 8)) {
     unsigned long pt;
-    if(1 == sscanf(cmd, "SLAVE 0x%lx", &pt)) {
+    if(1 == sscanf(cmd, "SLAVE 0x%lu", &pt)) {
       xine_stream_t *slave_stream = (xine_stream_t*)pt;
       if(this->slave_stream != slave_stream) {
 
