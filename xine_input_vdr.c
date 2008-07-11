9658fb1ede196e5360800d5fa8fb36921c665df2
Petri Hintukainen <phintuka@users.sourceforge.net>
2008-07-11 18:09:36 +0000

    Sync fifo_buffer_new with current xine-lib 1.2 hg (xine_xmalloc_aligned -> av_mallocz)

--- xine_input_vdr.c
+++ xine_input_vdr.c
@@ -40,6 +40,10 @@
 # include <scsi/sg.h>
 #endif
 
+#if XINE_VERSION_CODE >= 10190
+# include <libavutil/mem.h>
+#endif
+
 #include <xine/xine_internal.h>
 #include <xine/xineutils.h>
 #include <xine/input_plugin.h>
@@ -1384,7 +1388,6 @@ static fifo_buffer_t *fifo_buffer_new (xine_stream_t *stream, int num_buffers, u
   fifo_buffer_t *ref = stream->video_fifo;
   fifo_buffer_t *this;
   int            i;
-  int            alignment = 2048;
   unsigned char *multi_buffer = NULL;
 
   LOGDBG("fifo_buffer_new...");
@@ -1414,11 +1417,7 @@ static fifo_buffer_t *fifo_buffer_new (xine_stream_t *stream, int num_buffers, u
    * init buffer pool, allocate nNumBuffers of buf_size bytes each
    */
 
-  if (buf_size % alignment != 0)
-    buf_size += alignment - (buf_size % alignment);
-
-  multi_buffer = xine_xmalloc_aligned (alignment, num_buffers * buf_size,
-                                       &this->buffer_pool_base);
+  multi_buffer = this->buffer_pool_base = av_mallocz (num_buffers * buf_size);
 
   pthread_mutex_init (&this->buffer_pool_mutex, NULL);
   pthread_cond_init (&this->buffer_pool_cond_not_empty, NULL);
