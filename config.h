ad8b1edb4e9cc335d091fd00f8159aeefc98f189
Petri Hintukainen <phintuka@users.sourceforge.net>
2008-07-29 21:04:35 +0000

    Revert notes

--- config.h
+++ config.h
@@ -80,30 +80,6 @@
 #define X11_DRIVER_NONE          9
 #define X11_DRIVER_count         10
 
-#if 0
-class ConfEntry {
-  const enum target {vdr, xine_lib, xine_fe};
-  const enum type { integer, boolean, enumeration, string };
-  const char *key;      /* xineliboutput.xine.$xinekey */
-  const char *xine_key; /* xineliboutput.xine.$xinekey */
-  union {
-    char *strvalue;
-    int   intvalue;
-    bool  boolvalue;
-  };
-
-  //operator T() { return value; };
-  //operator *T() { return &value; };
-  void Store(void); /* store to Setup. Send to xine, etc. */
-  void Parse(const char *value);
-  bool Parse(const char *key, const char *value) {
-    if(strcasecmp(key, )) return false; Parse(); return true; }
-  //void Set(T value);
-};
-//ConfEntry<int> subtitle_offset;
-//ConfEntry<bool> subtitle_offset;
-#endif
-
 #define FB_DRIVER_AUTO           0
 #define FB_DRIVER_FB             1
 #define FB_DRIVER_DIRECTFB       2
