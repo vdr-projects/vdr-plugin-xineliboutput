2301f83b58106807ed9b86c6d3fb91df94adea5c
Petri Hintukainen <phintuka@users.sourceforge.net>
2008-10-26 07:35:17 +0000

    1.0.3: Quickfix: remove scaling artefacts from scaled HUD OSD: Use oversized source image for scaling

--- xine_sxfe_frontend.c
+++ xine_sxfe_frontend.c
@@ -467,10 +467,10 @@ void xrender_surf_blend(Display *dpy, Xrender_Surf *src, Xrender_Surf *dst,
   xf.matrix[2][0] = 0; xf.matrix[2][1] = 0; xf.matrix[2][2] = XDoubleToFixed(1.0);
   XRenderSetPictureFilter(dpy, src->pic, smooth ? "bilinear" : "nearest", NULL, 0);
   XRenderSetPictureTransform(dpy, src->pic, &xf);
-  x = (int)ceil((double)x * scale_x);
-  y = (int)ceil((double)y * scale_y);
-  w = (int)floor((double)w * scale_x);
-  h = (int)floor((double)h * scale_y);
+  x = (int)ceil((double)(x?x-1:0) * scale_x);
+  y = (int)ceil((double)(y?y-1:0) * scale_y);
+  w = (int)floor((double)(w+2) * scale_x);
+  h = (int)floor((double)(h+2) * scale_y);
   XRenderComposite(dpy, PictOpSrc, src->pic, None, dst->pic, x, y, 0, 0, x, y, w, h);
 }
 
