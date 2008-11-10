37cdaa65608a787e291e5561c30310641b195b04
Petri Hintukainen <phintuka@users.sourceforge.net>
2008-11-10 23:59:35 +0000

    Moved fe_t to header

--- xine_frontend.c
+++ xine_frontend.c
@@ -19,67 +19,14 @@
 #define LOG_MODULENAME "[vdr-fe]    "
 #include "logdefs.h"
 
+#include "xine_frontend_internal.h"
 #include "xine/post.h"
 
-/*
- * data
- */
-
-
-typedef struct fe_s {
-  /* function pointers */
-  frontend_t          fe;
-  void              (*update_display_size)    (struct fe_s *);
-  void              (*toggle_fullscreen_state)(struct fe_s *);
-
-  /* vdr callbacks */
-  fe_keypress_f       keypress;
-
-  /* xine stuff */
-  xine_t             *xine;
-  xine_stream_t      *stream;
-  xine_stream_t      *slave_stream;
-  vdr_input_plugin_if_t *input_plugin;
-  xine_video_port_t  *video_port;
-  xine_video_port_t  *video_port_none;
-  xine_audio_port_t  *audio_port;
-  xine_audio_port_t  *audio_port_none;
-  xine_event_queue_t *event_queue;
-
-  post_plugins_t     *postplugins;
-  char               *video_port_name;   /* frame buffer device */
-  char               *aspect_controller; /* path to external HW aspect ratio controller */
-  char               *configfile;        /* path of our config file */
-
-  int                 xine_visual_type;
-  union {
-    void             *vis;
-    fb_visual_t       vis_fb;
-    x11_visual_t      vis_x11;
-  };
-
-  /* frontend */
-  double      video_aspect;    /* aspect ratio of video frame */
-  double      display_ratio;   /* aspect ratio of video window */
-  uint16_t    xpos, ypos;      /* position of video window */
-  uint16_t    width;           /* size of video window */
-  uint16_t    height;          /* */
-  uint16_t    video_width;     /* size of video frame */
-  uint16_t    video_height;    /* */
-  uint16_t    pes_buffers;     /* max. number of PES packets in video fifo */
-  uint8_t     aspect;          /* aspect ratio of video window (user setting) */
-  uint8_t     overscan;        /* overscan in % (crop video borders) */
-/*uint8_t     cropping : 1;*/
-  uint8_t     scale_video : 1; /* enable/disable all video scaling */
-  uint8_t     field_order : 1; /* invert top/bottom field order */
-  uint8_t     playback_finished : 1;
-  uint8_t     slave_playback_finished : 1;
-
-} fe_t;
 
 static int verbose_xine_log = 0;
 
-static inline char *strn0cpy(char *dest, const char *src, int n) 
+
+char *strn0cpy(char *dest, const char *src, int n) 
 {
   char *s = dest;
   for ( ; --n && (*dest = *src) != 0; dest++, src++) ;
@@ -1217,7 +1164,7 @@ static int xine_queue_pes_packet(frontend_t *this_gen, const char *data, int len
 
 #else /* #ifndef FE_STANDALONE */
 
-static void process_xine_keypress(fe_t *this, 
+void process_xine_keypress(fe_t *this, 
 				  const char *map, const char *key,
 				  int repeat, int release)
 {
