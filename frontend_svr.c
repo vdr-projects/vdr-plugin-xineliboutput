697cd1e6cb686a9de8b7516e2f4dd7e569089163
Petri Hintukainen <phintuka@users.sourceforge.net>
2008-11-14 22:01:57 +0000

    Added "Server busy" response when there are no free client slots
    Silenced compiler warnings
      warning: ignoring return value of ‘ssize_t
      write(int, const void*, size_t)’, declared with attribute warn_unused_result

--- frontend_svr.c
+++ frontend_svr.c
@@ -1689,14 +1689,15 @@ void cXinelibServer::Handle_ClientConnected(int fd)
   bool accepted = SVDRPhosts.Acceptable(sin.sin_addr.s_addr);
   if(!accepted) {
     LOGMSG("Address not allowed to connect (svdrphosts.conf).");
-    write(fd, "Access denied.\r\n", 16);
+    (void)write(fd, "Access denied.\r\n", 16);
     CLOSESOCKET(fd);  
     return;    
   }
 
   if(cli>=MAXCLIENTS) {
     // too many clients
-    LOGMSG("Too mant clients, connection refused");
+    LOGMSG("Too many clients, connection refused");
+    (void)write(fd, "Server busy.\r\n", 16);
     CLOSESOCKET(fd);  
     return;
   }
