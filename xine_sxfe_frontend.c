5842f9b730e82dc5cbe673f0fa6a826870643b77
Rolf Ahrenberg <rofafor@users.sourceforge.net>
2009-01-02 17:54:02 +0000

    Fixed HAVE_XINERAMA in update_xinerama_info.

--- xine_sxfe_frontend.c
+++ xine_sxfe_frontend.c
@@ -439,10 +439,10 @@ static void set_cursor(Display *dpy, Window win, const int enable)
 
 static void update_xinerama_info(sxfe_t *this)
 {
+#ifdef HAVE_XINERAMA
   int screen = this->xinerama_screen;
   this->xinerama_x = this->xinerama_y = 0;
   XLockDisplay(this->display);
-#ifdef HAVE_XINERAMA
   if(screen >= -1 && XineramaIsActive(this->display)) {
     XineramaScreenInfo *screens;
     int num_screens;
