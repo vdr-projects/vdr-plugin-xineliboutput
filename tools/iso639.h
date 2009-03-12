450b5c97b22c0bb8ea12937fa58547a2b2d7058f
Petri Hintukainen <phintuka@users.sourceforge.net>
2009-03-12 08:32:59 +0000

    Added depreciated languagecode for hebrew (iw) (Thanks to Lauri Tischler)

--- tools/iso639.h
+++ tools/iso639.h
@@ -60,6 +60,7 @@ static const struct {
   {"guj", "gu"},
   {"hau", "ha"},
   {"heb", "he"},
+  {"heb", "iw"},
   {"hin", "hi"},
   {"hun", "hu"},
   {"ice", "is"},
@@ -157,7 +158,7 @@ static const struct {
 static const char *iso639_2_to_iso639_1(const char *lang)
 {
   if (lang && lang[0]) {
-    if(lang[1] && !lang[2]) {
+    if (lang[1] && !lang[2]) {
       for (unsigned int i = 0 ; i < sizeof(ISO639_map) / sizeof(ISO639_map[0]); i++)
 	if (((uint16_t*)ISO639_map[i].iso639_1)[0] == ((uint16_t*)lang)[0])
 	  return ISO639_map[i].iso639_2;
@@ -171,7 +172,7 @@ static const char *iso639_2_to_iso639_1(const char *lang)
 static const char *iso639_1_to_iso639_2(const char *lang)
 {
   if (lang && lang[0]) {
-    if(lang[1] && lang[2] && !lang[3]) {
+    if (lang[1] && lang[2] && !lang[3]) {
       for (unsigned int i = 0 ; i < sizeof(ISO639_map) / sizeof(ISO639_map[0]); i++)
 	if (((uint16_t*)ISO639_map[i].iso639_2)[0] == ((uint16_t*)lang)[0] &&
 	    ((uint8_t *)ISO639_map[i].iso639_2)[2] == ((uint8_t *)lang)[2])
