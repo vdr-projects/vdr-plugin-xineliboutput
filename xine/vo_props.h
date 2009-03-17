8d40ec97c0e9e58f9d15202a96f7f81b3e845857
Petri Hintukainen <phintuka@users.sourceforge.net>
2009-03-17 12:20:33 +0000

    Removed deprecated properties. Synced extended capabilities with current xine-lib 1.2. Added scaling setting to vdr_osd_extradata_t.

--- xine/vo_props.h
+++ xine/vo_props.h
@@ -11,26 +11,50 @@
 #ifndef XINELIBOUTPUT_VO_PROPS_H_
 #define XINELIBOUTPUT_VO_PROPS_H_
 
+/*
+ * Extended vo capabilities
+ */
 
 /* output can scale OSD */
-#define VO_CAP_OSDSCALING  0x01000000
+#ifdef VO_CAP_CUSTOM_EXTENT_OVERLAY
+  /* xine-lib 1.2 */
+# define VO_CAP_OSDSCALING  VO_CAP_CUSTOM_EXTENT_OVERLAY
+#else
+# define VO_CAP_OSDSCALING  0x01000000
+#endif
+
+/* Output can blend ARGB surfaces */
+#ifdef VO_CAP_ARGB_LAYER_OVERLAY
+# define VO_CAP_ARGB  VO_CAP_ARGB_LAYER_OVERLAY
+#else
+# define VO_CAP_ARGB  0x02000000
+#endif
+
+
+/*
+ * Extended vo properties
+ */
 
 /* enable/disable OSD scaling */
 #define VO_PROP_OSD_SCALING 0x1001
-/* OSD width */
-#define VO_PROP_OSD_WIDTH   0x1002
-/* OSD height */
-#define VO_PROP_OSD_HEIGHT  0x1003
 
-/* VDR OSD , hili_rgb_clut */
+
+/*
+ * VDR OSD tagging and extra data
+ */
+
+/* VDR OSD , stored in overlay hili_rgb_clut member */
 #define VDR_OSD_MAGIC       -9999
 
+/* VDR OSD extra data, stored in overlay hili clut data */
 typedef struct {
   /* extent of reference coordinate system */
   uint16_t extent_width;
   uint16_t extent_height;
   /* overlay layer */
   uint32_t layer;
+  /* scaling: 0 - disable , 1...N - quality */
+  uint8_t scaling;
 } vdr_osd_extradata_t;
 
 #endif /* XINELIBOUTPUT_VO_PROPS_H_ */
