d5461b505d3998106836297f4a2bf28fcbb0608e
Petri Hintukainen <phintuka@users.sourceforge.net>
2009-03-17 12:14:41 +0000

    Initial import

---
+++ xine/vo_osdreorder.h
@@ -0,0 +1,16 @@
+/*
+ * vo_osdreorder.h: OSD re-ordering video-out post plugin
+ *
+ * See the main source file 'xineliboutput.c' for copyright information and
+ * how to reach the author.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef _XINELIBOUTPUT_VO_OSDREORDER_H
+#define _XINELIBOUTPUT_VO_OSDREORDER_H
+
+vo_driver_t *osd_reorder_init(void);
+
+#endif /* _XINELIBOUTPUT_VO_OSDREORDER_H */
