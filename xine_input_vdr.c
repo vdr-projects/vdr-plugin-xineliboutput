ab2e66af9c3bd9f024eff2fe58bdf1fd8afc1f8a
Petri Hintukainen <phintuka@users.sourceforge.net>
2009-06-11 15:29:12 +0000

    Merge from trunk:   revision 1.150     Splitting large vdr_plugin_read_block (demux_buf)   revision 1.149     Splitting large vdr_plugin_read_block (postprocess_buf)   revision 1.148     Splitting large vdr_plugin_read_block (adjust_scr_speed)   revision 1.147     Splitting large vdr_plugin_read_block (handle_disconnect)

--- xine_input_vdr.c
+++ xine_input_vdr.c
@@ -5183,31 +5183,187 @@ buf_element_t *post_frame_h264(vdr_input_plugin_t *this, buf_element_t *buf)
 }
 #endif /* TEST_H264 */
 
+/*
+ * Demux some buffers not supported by mpeg_block demuxer:
+ *  - H.264 video
+ *  - DVB Subtitles
+ */
+static buf_element_t *demux_buf(vdr_input_plugin_t *this, buf_element_t *buf)
+{
+#ifdef TEST_H264
+  /* H.264 */
+  if (IS_VIDEO_PACKET(buf->content)) {
+    if (this->h264) {
+      if (this->h264 < 0)
+	this->h264 = detect_h264(this, buf->content, buf->size);
+      
+      if (this->h264 > 0)
+	buf = post_frame_h264(this, buf);
+    }
+    return buf;
+  }
+#endif
+
+#ifdef TEST_DVB_SPU
+  /* DVB subtitles */
+  if (buf->content[3] == PRIVATE_STREAM1) {
+  }
+#endif
+
+  return buf;
+}
+
+/*
+ * Postprocess buffer before passing it to demuxer
+ * - Track audio stream changes
+ * - Detect pts wraps
+ * - Signal new pts upstream after stream changes
+ * - Special handling for still images
+ * - Count video frames for SCR tunning
+ * - Special handling for ffmpeg mpeg2 video decoder
+ */
+static void postprocess_buf(vdr_input_plugin_t *this, buf_element_t *buf, int need_pause)
+{
+#ifdef CACHE_FIRST_IFRAME
+  cache_iframe(this, buf);
+#endif
+
+  track_audio_stream_change(this, buf);
+
+  pts_wrap_workaround(this, buf);
+
+  /* Send current PTS ? */
+  if(this->send_pts) {
+    int64_t pts = pes_get_pts(buf->content, buf->size);
+    if(pts > 0) {
+#ifdef TEST_SCR_PAUSE
+      if(need_pause)
+	scr_tunning_set_paused(this);
+#endif
+      vdr_x_demux_control_newpts(this->stream, pts, BUF_FLAG_SEEK);
+      this->send_pts = 0;
+    } else if(pts == 0) {
+      /* Still image? do nothing, leave send_pts ON */
+    }
+  }
+
+#ifdef LOG_FIRSTFRAME_FLAG
+  {
+    /* trace first frame flag changes */
+    static uint64_t timer = 0;
+    static int tfd = 0;
+    pthread_mutex_lock (&this->stream->first_frame_lock);
+    if(tfd != this->stream->first_frame_flag) {
+      uint64_t now = monotonic_time_ms();
+      if(tfd)
+	LOGMSG("FIRST FRAME FLAG %d -> %d (%d ms)", 
+	       tfd, this->stream->first_frame_flag, (int)(now-timer));
+      timer = now;
+      tfd = this->stream->first_frame_flag;
+    }
+    pthread_mutex_unlock (&this->stream->first_frame_lock);
+  }
+#endif
+
+  /* generated still images start with empty video PES, PTS = 0.
+     Reset metronom pts so images will be displayed */
+  if(this->still_mode && buf->size == 14) {
+    int64_t pts = pes_get_pts(buf->content, buf->size);
+    if(pts==0) {
+      vdr_x_demux_control_newpts(this->stream, pts, BUF_FLAG_SEEK);
+      /* delay frame 10ms (9000 ticks) */
+      /*buf->content[12] = (uint8_t)((10*90) >> 7);*/
+    }
+  }
+
+#ifndef FFMPEG_DEC
+
+  /* Count video frames for SCR tunning algorithm */
+  if(this->live_mode && this->I_frames < 4)
+    if(IS_VIDEO_PACKET(buf->content) && buf->size > 32)
+      update_frames(this, buf->content, buf->size);
+
+#else /* FFMPEG_DEC */
+
+  /* Count video frames for SCR tunning algorithm */
+  if(this->ffmpeg_video_decoder || (this->live_mode && this->I_frames < 4))
+    if(IS_VIDEO_PACKET(buf->content) && buf->size > 32) {
+      uint8_t type = update_frames(this, buf->content, buf->size);
+      if(type && this->ffmpeg_video_decoder) {
+	/* signal FRAME_END to decoder */
+	post_frame_end(this, buf);
+	/* for some reason ffmpeg mpeg2 decoder does not understand pts'es in B frames ? 
+	 * (B-frame pts's are smaller than in previous P-frame) 
+	 * Anyway, without this block of code B frames with pts are dropped. */
+	if(type == B_FRAME && PES_HAS_PTS(buf->content))
+	  pes_strip_pts(buf->content, buf->size);
+      }
+    }
+
+#endif
+}
+
+static void handle_disconnect(vdr_input_plugin_t *this)
+{
+  LOGMSG("read_block: no data source, returning NULL");
+  if(this->block_buffer)
+    this->block_buffer->clear(this->block_buffer);
+  if(this->big_buffer)
+    this->big_buffer->clear(this->big_buffer);
+  if(this->hd_buffer)
+    this->hd_buffer->clear(this->hd_buffer);
+  set_playback_speed(this, 1);
+  this->live_mode = 0;
+  reset_scr_tunning(this, XINE_FINE_SPEED_NORMAL);
+  this->stream->emergency_brake = 1;
+}
+
+static int adjust_scr_speed(vdr_input_plugin_t *this)
+{
+  int need_pause = 0;
+
+#ifdef ADJUST_SCR_SPEED
+  if(pthread_mutex_lock(&this->lock)) {
+    LOGERR("adjust_scr_speed: pthread_mutex_lock failed");
+    return 0;
+  }
+
+  if( (!this->live_mode && (this->fd_control < 0 || 
+			    this->fixed_scr)) ||
+      this->slave_stream) {
+    if(this->scr_tunning)
+      reset_scr_tunning(this, this->speed_before_pause);
+  } else {
+# ifdef TEST_SCR_PAUSE
+    if(this->stream_start || this->send_pts) {
+      reset_scr_tunning(this, this->speed_before_pause);
+      need_pause = 1;
+    } else {
+      vdr_adjust_realtime_speed(this);
+    }
+# else
+    vdr_adjust_realtime_speed(this);
+# endif
+  }
+  pthread_mutex_unlock(&this->lock); 
+#endif
+
+  return need_pause;
+}
+
 static buf_element_t *vdr_plugin_read_block (input_plugin_t *this_gen,
 					     fifo_buffer_t *fifo, off_t todo) 
 {
   vdr_input_plugin_t *this = (vdr_input_plugin_t *) this_gen;
   buf_element_t      *buf  = NULL;
-#ifdef TEST_SCR_PAUSE
-  int need_pause = 0;
-#endif
+  int need_pause;
 
   TRACE("vdr_plugin_read_block");
 
   /* check for disconnection/termination */
   if(!this->funcs.push_input_write /* reading from socket */ &&
      !this->control_running) {
-    LOGMSG("read_block: no data source, returning NULL");
-    if(this->block_buffer)
-      this->block_buffer->clear(this->block_buffer);
-    if(this->big_buffer)
-      this->big_buffer->clear(this->big_buffer);
-    if(this->hd_buffer)
-      this->hd_buffer->clear(this->hd_buffer);
-    set_playback_speed(this, 1);
-    this->live_mode = 0;
-    reset_scr_tunning(this, XINE_FINE_SPEED_NORMAL);
-    this->stream->emergency_brake = 1;
+    handle_disconnect(this);
     return NULL; /* disconnected ? */
   }
 
@@ -5224,31 +5380,7 @@ static buf_element_t *vdr_plugin_read_block (input_plugin_t *this_gen,
 #endif
 
   /* adjust SCR speed */
-#ifdef ADJUST_SCR_SPEED
-  if(pthread_mutex_lock(&this->lock)) {
-    LOGERR("read_block: pthread_mutex_lock failed");
-    return NULL;
-  }
-
-  if( (!this->live_mode && (this->fd_control < 0 || 
-			    this->fixed_scr)) ||
-      this->slave_stream) {
-    if(this->scr_tunning)
-      reset_scr_tunning(this, this->speed_before_pause);
-  } else {
-# ifdef TEST_SCR_PAUSE
-    if(this->stream_start || this->send_pts) {
-      reset_scr_tunning(this, this->speed_before_pause);
-      need_pause = 1;
-    } else {
-# endif
-      vdr_adjust_realtime_speed(this);
-# ifdef TEST_SCR_PAUSE
-    }
-# endif
-  }
-  pthread_mutex_unlock(&this->lock); 
-#endif
+  need_pause = adjust_scr_speed(this);
 
   do {
     buf = fifo_buffer_try_get(this->block_buffer);
@@ -5345,94 +5477,12 @@ static buf_element_t *vdr_plugin_read_block (input_plugin_t *this_gen,
 
     pthread_mutex_unlock(&this->lock);
 
-#ifdef TEST_H264
-    /* H.264 */
-    if(this->h264 && (buf->content[3] & 0xf0) == 0xe0) {
-      if(this->h264 < 0)
-	this->h264 = detect_h264(this, buf->content, buf->size);
-      
-      if(this->h264 > 0) {
-	buf = post_frame_h264(this, buf);
-	if(!buf)
-	  continue;
-      }
-    }
-#endif
+    buf = demux_buf(this, buf);
 
   } while(!buf);
 
-#ifdef CACHE_FIRST_IFRAME
-  cache_iframe(this, buf);
-#endif
-
-  track_audio_stream_change(this, buf);
-
-  pts_wrap_workaround(this, buf);
-
-  /* Send current PTS ? */
-  if(this->send_pts) {
-    int64_t pts = pes_get_pts(buf->content, buf->size);
-    if(pts > 0) {
-#ifdef TEST_SCR_PAUSE
-      if(need_pause)
-	scr_tunning_set_paused(this);
-#endif
-      vdr_x_demux_control_newpts(this->stream, pts, BUF_FLAG_SEEK);
-      this->send_pts = 0;
-    } else if(pts == 0) {
-      /* Still image? do nothing, leave send_pts ON */
-    }
-  }
-
-#ifdef LOG_FIRSTFRAME_FLAG
-  {
-    /* trace flag changes */
-    static uint64_t timer = 0;
-    static int tfd = 0;
-    pthread_mutex_lock (&this->stream->first_frame_lock);
-    if(tfd != this->stream->first_frame_flag) {
-      uint64_t now = monotonic_time_ms();
-      if(tfd)
-	LOGMSG("FIRST FRAME FLAG %d -> %d (%d ms)", 
-	       tfd, this->stream->first_frame_flag, (int)(now-timer));
-      timer = now;
-      tfd = this->stream->first_frame_flag;
-    }
-    pthread_mutex_unlock (&this->stream->first_frame_lock);
-  }
-#endif
-
-  if(this->still_mode && buf->size == 14) {
-    /* generated still images start with empty video PES, PTS = 0.
-       Reset metronom pts so images will be displayed */
-    int64_t pts = pes_get_pts(buf->content, buf->size);
-    if(pts==0) {
-      vdr_x_demux_control_newpts(this->stream, pts, BUF_FLAG_SEEK);
-      /* delay frame 10ms (9000 ticks) */
-      /*buf->content[12] = (uint8_t)((10*90) >> 7);*/
-    }
-  }
-
-#ifndef FFMPEG_DEC
-  if(this->live_mode && this->I_frames < 4)
-    if(IS_VIDEO_PACKET(buf->content) && buf->size > 32)
-      update_frames(this, buf->content, buf->size);
-#else /* FFMPEG_DEC */
-  if(this->ffmpeg_video_decoder || (this->live_mode && this->I_frames < 4))
-    if(IS_VIDEO_PACKET(buf->content) && buf->size > 32) {
-      uint8_t type = update_frames(this, buf->content, buf->size);
-      if(type && this->ffmpeg_video_decoder) {
-	/* signal FRAME_END to decoder */
-	post_frame_end(this, buf);
-	/* for some reason ffmpeg mpeg2 decoder does not understand pts'es in B frames ? 
-	 * (B-frame pts's are smaller than in previous P-frame) 
-	 * Anyway, without this block of code B frames with pts are dropped. */
-	if(type == B_FRAME && PES_HAS_PTS(buf->content))
-	  pes_strip_pts(buf->content, buf->size);
-      }
-    }
-#endif
-
+  postprocess_buf(this, buf, need_pause);
+  
   TRACE("vdr_plugin_read_block: return data, pos end = %" PRIu64, this->curpos);
   return buf;
 }
