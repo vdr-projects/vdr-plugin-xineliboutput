1fa68c514e8c1c6a590284d4e3dee7efabaf986e
cvs2svn <admin@example.com>
2009-09-25 12:24:05 +0000

    This commit was manufactured by cvs2svn to create branch 'branch-1_0_x'.
    Cherrypick from master 2009-09-25 12:24:04 UTC Petri Hintukainen <phintuka@users.sourceforge.net> 'Removed trailing spaces':
        frontend_local.h
        frontend_svr.h
        logdefs.h
        po/zh_CN.po
        po/zh_TW.po
        tools/backgroundwriter.c
        tools/backgroundwriter.h
        tools/pes.c
        tools/pes.h
        tools/udp_pes_scheduler.c
        tools/udp_pes_scheduler.h
        xine_input_vdr_net.h

---
+++ frontend_local.h
@@ -0,0 +1,70 @@
+/*
+ * frontend_local.h:
+ *
+ * See the main source file 'xineliboutput.c' for copyright information and
+ * how to reach the author.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef __XINELIB_FRONTEND_LOCAL_H
+#define __XINELIB_FRONTEND_LOCAL_H
+
+#include "frontend.h"
+
+//----------------------------- cXinelibLocal --------------------------------
+
+extern "C" {
+  typedef struct frontend_s frontend_t;
+}
+
+class cXinelibLocal : public cXinelibThread
+{
+
+  public:
+    cXinelibLocal(const char *frontend_name);
+    virtual ~cXinelibLocal();
+
+  protected:
+    virtual void Action(void);
+
+  public:
+
+    // Data transfer
+    virtual int     Play_PES(const uchar *buf, int len);
+    virtual void    OsdCmd(void *cmd);
+    virtual int64_t GetSTC();
+
+    // Playback files
+    virtual bool EndOfStreamReached(void);
+
+    // Image grabbing
+    virtual uchar *GrabImage(int &Size, bool Jpeg, int Quality,
+			     int SizeX, int SizeY);
+
+    // Configuration
+    virtual void ConfigureWindow(int fullscreen, int width, int height,
+				 int modeswitch, const char *modeline,
+				 int aspect, int scale_video, int field_order);
+    virtual void ConfigureDecoder(int pes_buffers);
+
+  protected:
+
+    // Playback control
+    virtual int  Xine_Control(const char *cmd);
+
+  protected:
+
+    // Frontend access
+    frontend_t *load_frontend(const char *fe_name);
+
+    // Data
+    void       *h_fe_lib;
+    frontend_t *fe;
+    cRwLock     m_feLock;
+    bool        m_bReconfigRequest;
+};
+
+
+#endif // __XINELIB_FRONTEND_LOCAL_H
