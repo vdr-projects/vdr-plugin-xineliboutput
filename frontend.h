e21e35e273570d4f57c69851b049e15f1a934644
Petri Hintukainen <phintuka@users.sourceforge.net>
2010-01-16 20:55:05 +0000

    Added Sync(): wait until all pending control messages have been processed by the client

--- frontend.h
+++ frontend.h
@@ -52,6 +52,9 @@ class cXinelibThread : public cThread, public cListObject
 
     virtual void TrickSpeed(int Speed, bool Backwards);
 
+    // Sync(): wait until all pending control messages have been processed by the client
+    virtual void Sync(void) { return Xine_Control("SYNC"); };
+
   protected:
     int  Xine_Control(const char *cmd, const char *p1);
     int  Xine_Control(const char *cmd, int p1);
