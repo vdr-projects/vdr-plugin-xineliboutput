069ed56cf036f94e7295dcc0b99631859f6182f0
Petri Hintukainen <phintuka@users.sourceforge.net>
2010-02-27 16:32:24 +0000

    Moved TsBufferFlush() in DeviceStillPicture() to right place

--- device.c
+++ device.c
@@ -1382,6 +1382,7 @@ void cXinelibDevice::StillPicture(const uchar *Data, int Length)
         else
           cCondWait::SleepMs(5);
       }
+      TsBufferFlush();
 #endif
     } else {
       ForEach(m_clients, &cXinelibThread::Play_Mpeg2_ES,
@@ -1389,12 +1390,12 @@ void cXinelibDevice::StillPicture(const uchar *Data, int Length)
               &mand<bool>, true);
     }
 
+  TsBufferFlush();
+
   // creates empty video PES with pseudo-pts
   ForEach(m_clients, &cXinelibThread::Play_Mpeg2_ES,
           Data, 0, VIDEO_STREAM, isH264,
           &mand<bool>, true);
-
-  TsBufferFlush();
 
   ForEach(m_clients, &cXinelibThread::Flush, 60, &mand<bool>, true);
 
