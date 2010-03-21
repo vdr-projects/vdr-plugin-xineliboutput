881f67b0255b9aabe6d79e7a648abc5a243b9709
Petri Hintukainen <phintuka@users.sourceforge.net>
2010-03-21 10:26:47 +0000

    wire()/unwire(): safety checks (stream, stream->metronom)

--- xine/xvdr_metronom.c
+++ xine/xvdr_metronom.c
@@ -174,17 +174,27 @@ static void xvdr_metronom_set_still_mode(xvdr_metronom_t *this, int still_mode)
 
 static void xvdr_metronom_wire(xvdr_metronom_t *this)
 {
+  if (!this->stream) {
+    LOGMSG("xvdr_metronom_wire(): stream == NULL !");
+    return;
+  }
+  if (!this->stream->metronom) {
+    LOGMSG("xvdr_metronom_wire(): stream->metronom == NULL !");
+    return;
+  }
+
   if (!this->wired) {
     this->wired = 1;
 
     /* attach to stream */
+    this->orig_metronom = this->stream->metronom;
     this->stream->metronom = &this->metronom;
   }
 }
 
 static void xvdr_metronom_unwire(xvdr_metronom_t *this)
 {
-  if (this->wired) {
+  if (this->stream && this->wired) {
     this->wired = 0;
 
     /* detach from stream */
