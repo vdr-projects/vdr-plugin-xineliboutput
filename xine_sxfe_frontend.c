fbea3b7cad1af6245b481b0eeafe47232ce70b31
Petri Hintukainen <phintuka@users.sourceforge.net>
2010-05-21 12:33:08 +0000

    Floating point variables cannot be reliable compared using '!<float>'. (Thanks to Winfried Koehler)

--- xine_sxfe_frontend.c
+++ xine_sxfe_frontend.c
@@ -15,6 +15,7 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <math.h>
+#include <float.h>  /* DBL_MIN */
 
 /* X11 */
 #include <X11/Xlib.h>
@@ -532,9 +533,9 @@ static void xrender_surf_blend(Display *dpy, Xrender_Surf *src, Xrender_Surf *ds
 {
   XTransform xf;
 
-  if(!scale_x)
+  if (scale_x <= 2.0 * DBL_MIN)
     scale_x = 1;
-  if(!scale_y)
+  if (scale_y <= 2.0 * DBL_MIN)
     scale_y = 1;
 
   xf.matrix[0][0] = XDoubleToFixed(1.0 / scale_x); xf.matrix[0][1] = 0; xf.matrix[0][2] = 0;
