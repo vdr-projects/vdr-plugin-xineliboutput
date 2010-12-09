c83ddaf68cc028b7db12421757a63ccd351d7a51
Petri Hintukainen <phintuka@users.sourceforge.net>
2010-12-09 11:18:14 +0000

    Changed false --> NULL for cFileListItem() subfile pointer argument

--- menu.c
+++ menu.c
@@ -408,7 +408,7 @@ bool cMenuBrowseFiles::ScanDir(const char *DirName)
             if (m_Mode == ShowImages || m_Mode == ShowMusic)
               Add(new cFileListItem(e->d_name, true));
             else
-              Add(new cFileListItem(e->d_name, true, false, false,
+              Add(new cFileListItem(e->d_name, true, false, NULL,
                                     xc.IsDvdFolder(buffer), xc.IsBluRayFolder(buffer)));
 
           // regular files
@@ -424,7 +424,7 @@ bool cMenuBrowseFiles::ScanDir(const char *DirName)
 
             // DVD image (.iso)
             } else if (m_Mode == ShowFiles && xc.IsDvdImage(buffer)) {
-              Add(new cFileListItem(e->d_name, false, false, false, true));
+              Add(new cFileListItem(e->d_name, false, false, NULL, true));
 
             // video
             } else if (m_Mode == ShowFiles && xc.IsVideoFile(buffer)) {
