845d736c75e48250439dbb8f6e5706c2f2717893
cvs2svn <admin@example.com>
2011-01-16 12:48:45 +0000

    This commit was manufactured by cvs2svn to create branch 'branch-1_0_x'.
    Cherrypick from master 2010-12-09 12:19:25 UTC Petri Hintukainen <phintuka@users.sourceforge.net> 'Store cDvdMenu pointer in cOsdMenu*':
        logdefs.c
        media_player.h
        tools/cxsocket.c
        tools/cxsocket.h
        tools/listiter.h
        tools/rtcp.h
        tools/rtp.h
        tools/sap.h
        tools/ts.c
        tools/ts.h
        xine/BluRay/Makefile
        xine/post.c
        xine_frontend_lirc.c
    Cherrypick from master 2011-01-16 12:48:44 UTC Petri Hintukainen <phintuka@users.sourceforge.net> 'Added new OSD commands:':
        xine_osd_command.h

---
+++ media_player.h
@@ -0,0 +1,129 @@
+/*
+ * media_player.h: Media and image players
+ *
+ * See the main source file 'xineliboutput.c' for copyright information and
+ * how to reach the author.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef __XINELIB_PLAYER_H
+#define __XINELIB_PLAYER_H
+
+#include <vdr/player.h>
+
+#include "config.h"
+
+// --- Media player ---------------------------------------------------------
+
+class cXinelibPlayer;
+class cSkinDisplayReplay;
+class cPlaylistMenu;
+
+class cXinelibPlayerControl : public cControl 
+{
+  private:
+    static cMutex m_Lock;
+
+    static cXinelibPlayer *OpenPlayer(const char *File, bool Queue = false, const char *SubFile = NULL);
+
+ protected:
+    static cXinelibPlayer *m_Player;
+
+    cSkinDisplayReplay *m_DisplayReplay;
+    cPlaylistMenu      *m_PlaylistMenu;
+
+    void     CloseMenus(void);
+
+    eMainMenuMode m_Mode;
+    bool   m_ShowModeOnly;
+    bool   m_RandomPlay;
+    time_t m_AutoShowStart;
+    int    m_CurrentPos;
+    int    m_CurrentLen;
+    bool   m_BlinkState;
+
+    cTimeMs lastTime;
+    int number;
+
+    void MsgReplaying(const char *Title, const char *File);
+
+  public:
+    cXinelibPlayerControl(eMainMenuMode Mode, const char *File, const char *SubFile = NULL);
+    virtual ~cXinelibPlayerControl();
+
+    virtual void Show(void);
+    virtual void Hide(void);
+    virtual eOSState ProcessKey(eKeys Key);
+
+    virtual cOsdObject *GetInfo(void);
+
+    static void Close(void);
+    static bool IsOpen(void) { return m_Player != NULL; };
+    static void Queue(const char *File);
+};
+
+
+// --- DVD player -----------------------------------------------------------
+
+class cXinelibDvdPlayerControl : public cXinelibPlayerControl
+{
+  private:
+    cOsdMenu *m_DvdMenu;
+
+    void      CloseDvdMenu(void);
+
+  public:
+    cXinelibDvdPlayerControl(const char *File) :
+      cXinelibPlayerControl(ShowFiles, File), m_DvdMenu(NULL)
+      {}
+    virtual ~cXinelibDvdPlayerControl();
+
+    virtual void Show(void);
+    virtual void Hide(void);
+    virtual eOSState ProcessKey(eKeys Key);
+};
+
+// --- Image player ---------------------------------------------------------
+
+class cXinelibImagePlayer;
+
+class cXinelibImagesControl : public cControl 
+{
+  private:
+    static cXinelibImagePlayer *m_Player;
+    static cMutex m_Lock;
+
+    cSkinDisplayReplay *m_DisplayReplay;
+
+    char **m_Files;
+    char *m_File;
+    int m_Index;
+    int m_Count;
+    int m_Speed;
+    int m_LastShowTime;
+    bool m_ShowModeOnly;
+
+    static cXinelibImagePlayer *OpenPlayer(const char *File);
+
+  protected:
+    void Seek(int Rel);
+    void Delete(void);
+
+  public:
+    cXinelibImagesControl(char **Files, int Index, int Count);
+    virtual ~cXinelibImagesControl();
+
+    virtual void Show(void);
+    virtual void Hide(void);
+    virtual eOSState ProcessKey(eKeys Key);
+
+    virtual cOsdObject *GetInfo(void);
+
+    static void Close(void);
+    static bool IsOpen(void) { return m_Player != NULL; }
+};
+
+#endif // __XINELIB_PLAYER_H
+
