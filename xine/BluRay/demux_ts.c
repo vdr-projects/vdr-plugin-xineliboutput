e34de9fb2b1caafd4915880f53a8adaefee7fd18
Petri Hintukainen <phintuka@users.sourceforge.net>
2011-07-21 11:39:26 +0000

    Removed unused code and variables

--- xine/BluRay/demux_ts.c
+++ xine/BluRay/demux_ts.c
@@ -287,7 +287,6 @@ typedef struct {
   buf_element_t   *buf;
   unsigned int     counter;
   uint16_t         descriptor_tag; /* +0x100 for PES stream IDs (no available TS descriptor tag?) */
-  int64_t          packet_count;
   int              corrupted_pes;
   uint32_t         buffered_bytes;
   int              autodetected;
@@ -333,7 +332,6 @@ typedef struct {
   int              pkt_size;   /* TS packet size */
   int              pkt_offset; /* TS packet offset */
 
-  int              blockSize;
   int              rate;
   int              media_num;
   demux_ts_media   media[MAX_PIDS];
@@ -348,17 +346,12 @@ typedef struct {
    * and audio PIDs, we keep the index of the corresponding entry
    * inthe media[] array.
    */
-  unsigned int     programNumber;
-  unsigned int     pcrPid;
-  unsigned int     pid;
-  unsigned int     pid_count;
   unsigned int     videoPid;
   unsigned int     videoMedia;
 
   demux_ts_audio_track audio_tracks[MAX_AUDIO_TRACKS];
   int              audio_tracks_count;
 
-  int              send_end_buffers;
   int64_t          last_pts[2];
   int              send_newpts;
   int              buf_flag_seek;
@@ -1652,6 +1645,7 @@ printf("Program Number is %i, looking for %i\n",program_number,this->program_num
     section_length -= coded_length;
   }
 
+#if 0
   /*
    * Get the current PCR PID.
    */
@@ -1667,6 +1661,7 @@ printf("Program Number is %i, looking for %i\n",program_number,this->program_num
 #endif
     this->pcrPid = pid;
   }
+#endif
 
   if ( this->stream->spu_channel>=0 && this->spu_langs_count>0 )
     demux_ts_update_spu_channel( this );
@@ -1819,6 +1814,7 @@ static unsigned char * demux_synchronise(demux_ts_t* this) {
 }
 
 
+#ifdef TS_LOG
 static int64_t demux_ts_adaptation_field_parse(uint8_t *data,
 					       uint32_t adaptation_field_length) {
 
@@ -1904,6 +1900,7 @@ static int64_t demux_ts_adaptation_field_parse(uint8_t *data,
 #endif
   return PCR;
 }
+#endif
 
 /* check if an apid is in the list of known apids */
 static int apid_check(demux_ts_t*this, unsigned int pid) {
@@ -1994,9 +1991,11 @@ static void demux_ts_parse_packet (demux_ts_t*this) {
 
   if( adaptation_field_control & 0x2 ){
     uint32_t adaptation_field_length = originalPkt[4];
+#ifdef TS_LOG
     if (adaptation_field_length > 0) {
       demux_ts_adaptation_field_parse (originalPkt+5, adaptation_field_length);
     }
+#endif
     /*
      * Skip adaptation header.
      */
@@ -2257,7 +2256,6 @@ static void demux_ts_send_headers (demux_plugin_t *this_gen) {
 
   this->status = DEMUX_OK ;
 
-  this->send_end_buffers  = 1;
   this->scrambled_npids   = 0;
 
   /* DVBSUB */
@@ -2566,7 +2564,6 @@ static demux_plugin_t *open_plugin (demux_class_t *class_gen,
   this            = calloc(1, sizeof(*this));
   this->stream    = stream;
   this->input     = input;
-  this->blockSize = PKT_SIZE;
 
   this->demux_plugin.send_headers      = demux_ts_send_headers;
   this->demux_plugin.send_chunk        = demux_ts_send_chunk;
@@ -2594,8 +2591,6 @@ static demux_plugin_t *open_plugin (demux_class_t *class_gen,
     this->pmt_write_ptr[i]           = NULL;
   }
 
-  this->programNumber = INVALID_PROGRAM;
-  this->pcrPid = INVALID_PID;
   this->scrambled_npids = 0;
   this->videoPid = INVALID_PID;
   this->audio_tracks_count = 0;
