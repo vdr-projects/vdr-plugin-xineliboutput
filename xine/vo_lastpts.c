3ce00f552de30d167697fa4dd489f5e24f95dae0
Petri Hintukainen <phintuka@users.sourceforge.net>
2011-10-25 07:19:29 +0000

    vo_lastpts: added sanity checks and logging

--- xine/vo_lastpts.c
+++ xine/vo_lastpts.c
@@ -18,12 +18,17 @@
 
 #include "vo_hook.h"
 
+#define LOG_MODULENAME "[lastpts  ] "
+#include "../logdefs.h"
+
+
 /*
  *  lastpts_hook_t
  */
 typedef struct {
   vo_driver_hook_t  h;
   xine_stream_t    *prev_stream;
+  xine_stream_t    *xvdr_stream;
   metronom_t       *xvdr_metronom;
 } lastpts_hook_t;
 
@@ -39,27 +44,35 @@ static void lastpts_display_frame(vo_driver_t *self, vo_frame_t *vo_img)
 {
   lastpts_hook_t *this = (lastpts_hook_t*)self;
 
-#if 0
-  if (vo_img->stream) {
-    vo_img->stream->metronom->set_option(vo_img->stream->metronom, XVDR_METRONOM_LAST_VO_PTS, vo_img->pts);
-  }
-#else
+  ASSERT_RET(self,   return);
+  ASSERT_RET(vo_img, return);
 
   /* detect intercepted metronom with XVDR_* option support.
    * This prevents flooding log with "unknown option in set_option" messages
    */
-  if (vo_img->stream != this->prev_stream && vo_img->stream) {
+  if (vo_img->stream != this->prev_stream && vo_img->stream && vo_img->stream != XINE_ANON_STREAM) {
+    LOGMSG("stream changed from %p to %p", this->prev_stream, vo_img->stream);
     this->prev_stream = vo_img->stream;
+
     if (vo_img->stream->metronom->get_option(vo_img->stream->metronom, XVDR_METRONOM_ID) == XVDR_METRONOM_ID) {
+      LOGMSG("new stream is vdr stream");
       this->xvdr_metronom = vo_img->stream->metronom;
+      this->xvdr_stream   = vo_img->stream;
     }
   }
 
   if (this->xvdr_metronom) {
-    this->xvdr_metronom->set_option(this->xvdr_metronom, XVDR_METRONOM_LAST_VO_PTS, vo_img->pts);
-  }
 
-#endif
+    if (vo_img->stream == this->xvdr_stream) {
+      LOGVERBOSE("last pts %"PRId64, vo_img->pts);
+      ASSERT_RET(this->xvdr_metronom->set_option, return);
+
+      this->xvdr_metronom->set_option(this->xvdr_metronom, XVDR_METRONOM_LAST_VO_PTS, vo_img->pts);
+
+    } else {
+      LOGVERBOSE("last pts %"PRId64" - %p not vdr stream", vo_img->pts, vo_img->stream);
+    }
+  }
 
   this->h.orig_driver->display_frame(this->h.orig_driver, vo_img);
 }
