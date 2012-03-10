2295ee432afc59749eb3f49432711fb430392b37
Rolf Ahrenberg <rofafor@users.sourceforge.net>
2012-03-10 20:54:06 +0000

    Added support for vdr-1.7.26.

--- device.c
+++ device.c
@@ -77,7 +77,11 @@ class cXinelibStatusMonitor : public cStatus
     };
 
   protected:
+#if VDRVERSNUM < 10726
     virtual void ChannelSwitch(const cDevice *Device, int ChannelNumber);
+#else
+    virtual void ChannelSwitch(const cDevice *Device, int ChannelNumber, bool LiveView);
+#endif
     virtual void Replaying(const cControl *Control, const char *Name, 
 			   const char *FileName, bool On);
 
@@ -112,7 +116,11 @@ class cXinelibStatusMonitor : public cStatus
 };
 
 void cXinelibStatusMonitor::ChannelSwitch(const cDevice *Device, 
+#if VDRVERSNUM < 10726
 					  int ChannelNumber) 
+#else
+					  int ChannelNumber, bool LiveView) 
+#endif
 {
   TRACEF("cXinelibStatusMonitor::ChannelSwitch");
   TRACK_TIME(200);
