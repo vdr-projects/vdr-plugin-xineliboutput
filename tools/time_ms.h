7f2c92cdc1abc730543c607372122af4340e7630
Petri Hintukainen <phintuka@users.sourceforge.net>
2012-03-20 09:10:33 +0000

    time_ms(): use xine_monotonic_clock() when available

--- tools/time_ms.h
+++ tools/time_ms.h
@@ -16,7 +16,11 @@
 static uint64_t time_ms(void)
 {
   struct timeval t;
+#ifdef XINEUTILS_H
+  if (xine_monotonic_clock(&t, NULL) == 0)
+#else
   if (gettimeofday(&t, NULL) == 0)
+#endif
      return ((uint64_t)t.tv_sec) * 1000ULL + t.tv_usec / 1000ULL;
   return 0;
 }
