76c4add231d84d794016c46d206bafce7fb02542
Petri Hintukainen <phintuka@users.sourceforge.net>
2013-08-20 12:12:24 +0000

    Start 2.0.0

--- xineliboutput.c
+++ xineliboutput.c
@@ -43,7 +43,7 @@
 
 //---------------------------------plugin-------------------------------------
 
-static const char *VERSION        = "1.1.0";
+static const char *VERSION        = "2.0.0-cvs";
 static const char *DESCRIPTION    = trNOOP("X11/xine-lib output plugin");
 static const char *MAINMENUENTRY  = trNOOP("Media Player");
 
