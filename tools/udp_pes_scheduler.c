f28c964057843ceebf27203a742089defae3ea86
Petri Hintukainen <phintuka@gmail.com>
2017-01-12 11:25:23 +0200

    Fix uninitialized data

--- tools/udp_pes_scheduler.c
+++ tools/udp_pes_scheduler.c
@@ -228,6 +228,7 @@ bool cUdpScheduler::AddRtp(void)
 
   if(xc.remote_local_ip[0]) {
     struct sockaddr_in name;
+    memset(&name, 0, sizeof(name));
     name.sin_family = AF_INET;
     name.sin_addr.s_addr = inet_addr(xc.remote_local_ip);
     name.sin_port = htons(xc.remote_rtp_port+1);
